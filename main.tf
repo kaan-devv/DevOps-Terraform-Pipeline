09c80ad81001ce5b92425cee433ae354c65d9001
Kaan Yılmaz <kaan@yilbim.com.tr>
2025-11-11 11:16:03 +0300

    [DP-8] Name Change to v2 Test

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ resource "aws_s3_bucket" "inventory_bucket" {
   acl    = "private"
 }
 
-resource "aws_instance" "prodname_server" {
+resource "aws_instance" "prodv2_server" {
   ami           = "ami-080e1f13689e07408" 
   instance_type = "t3.micro" 
   tags = {
-    Name        = "prodname-server"
+    Name        = "prodv2-server"
     Service     = "inventory-panel-prod" 
   }
 }
 
 output "prod_server_details" {
   value = {
-    service_name  = aws_instance.prod_server.tags.Service
+    service_name  = aws_instance.prodv2_server.tags.Service
     instance_id   = aws_instance.prod_server.id
     instance_type = aws_instance.prod_server.instance_type
     public_ip     = aws_instance.prod_server.public_ip
