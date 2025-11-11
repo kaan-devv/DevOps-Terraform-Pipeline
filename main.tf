aea4d9e8197eb1bc700ff8b320cbb7e7e1634a0e
Kaan Yılmaz <kaan@yilbim.com.tr>
2025-11-11 11:28:27 +0300

    [DP-11] Name Change Test Correction

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ resource "aws_s3_bucket" "inventory_bucket" {
   acl    = "private"
 }
 
-resource "aws_instance" "prod_v2_server" {
+resource "aws_instance" "prod_server" {
   ami           = "ami-080e1f13689e07408" 
   instance_type = "t3.micro" 
   tags = {
     Name        = "prod-server-v2"
-    Service     = "inventory-panel-prod" 
+    Service     = "prod-server-v2" 
   }
 }
 
 output "prod_server_details" {
   value = {
-    service_name  = aws_instance.prod_v2_server.tags.Service
+    service_name  = aws_instance.prod_server_v2.tags.Service
     instance_id   = aws_instance.prod_server.id
     instance_type = aws_instance.prod_server.instance_type
     public_ip     = aws_instance.prod_server.public_ip
